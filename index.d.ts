daf7625131386b6b8ab2040728ccb4cf16cad82a
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-17 23:10:03 +0100

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+/**
+[FNV-1a](https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function) non-cryptographic hash function.
+*/
+export default function fnv1a(string: string): number;
