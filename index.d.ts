d5627c1cbc03779a615bf516812ee445651b7505
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-18 14:35:39 +0700

    Fix typo in the TS definition

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /**
 [FNV-1a](https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function) non-cryptographic hash function.
 
-@return The hash as a positive integer.
+@returns The hash as a positive integer.
 */
 export default function fnv1a(string: string): number;
